cb24ad28cd22de0e24e0d5536411fee3aef11697
unknown <matheusdasilvagastaldi@gmail.com>
2025-03-31 13:23:42 -0300

    Colocando a imagem do tesouro

--- app/src/main/java/com/example/cacatesouro/screen/TreasureScreen.kt
+++ app/src/main/java/com/example/cacatesouro/screen/TreasureScreen.kt
@@ -1,17 +1,24 @@
 package com.example.cacatesouro.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.cacatesouro.R
 
 @Composable
 fun TreasureScreen(navController: NavController){
@@ -23,9 +30,18 @@ fun TreasureScreen(navController: NavController){
 
         Text(
             text = "Parabéns! Você encontrou o tesouro!",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(bottom = 16.dp)
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 6.dp)
         )
+
+        Image(
+            painter = painterResource(id = R.drawable.baudeouro),
+            contentDescription = "Imagem do Tesouro",
+            modifier = Modifier
+                .size(200.dp)
+                .padding(16.dp)
+        )
+
         Button(onClick = { navController.navigate("home") }) {
             Text("Recomeçar")
         }
@@ -33,8 +49,8 @@ fun TreasureScreen(navController: NavController){
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun TesouroPreview(){
-//    TreasureScreen(navController = rememberNavController())
-//}
+@Preview(showBackground = true)
+@Composable
+fun TesouroPreview(){
+    TreasureScreen(navController = rememberNavController())
+}
